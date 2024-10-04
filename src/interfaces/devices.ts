5aaba24e97c91286cfcdab5314007cb4963f4bac
miikuru002 <jamutaqortegavelez@gmail.com>
2024-10-04 00:44:31 -0500

    add: devices view

--- src/interfaces/devices.ts
+++ src/interfaces/devices.ts
@@ -5,6 +5,7 @@ export interface IDevice {
   brand: string;
   model: string;
   specs: IDeviceSpecs;
+  type: keyof typeof EDeviceType;
 }
 
 export interface IDeviceSpecs {
@@ -12,4 +13,12 @@ export interface IDeviceSpecs {
   connectivity: string;
   compatibility: string;
   warranty: string;
+}
+
+export enum EDeviceType {
+  SENSOR = 'SENSOR',
+  ACTUATOR = 'ACTUATOR',
+  CONTROLLER = 'CONTROLLER',
+  DISPLAY = 'DISPLAY',
+  CAMERA = 'CAMERA'
 }
