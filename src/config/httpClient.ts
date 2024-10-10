dce5b44e7732c32f07e707a6056ae0fec6113da6
miikuru002 <jamutaqortegavelez@gmail.com>
2024-10-10 01:02:13 -0500

    refactor: httpClient to handle user authentication and token

--- src/config/httpClient.ts
+++ src/config/httpClient.ts
@@ -1,17 +1,18 @@
+import { authService } from '@/service/AuthService';
 import axios, { AxiosError } from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:8080/api'
 });
 
-//interceptor de solicitud -> se ejecuta antes de cada solicitud 
+//request interceptor -> it executes before the request promise is resolved
 httpClient.interceptors.request.use(
   async (config) => {
-    //se maneja la autenticación de usuario
-    // const user = await userManager.getUser();
-    // if (user) {
-    //   config.headers.Authorization = `Bearer ${user.access_token}`;
-    // }
+    //handle user authentication & token
+    const user = await authService.getUser();
+    if (user) {
+      config.headers.Authorization = `Bearer ${user.access_token}`;
+    }
     return config;
   },
   (error) => {
@@ -19,46 +20,44 @@ httpClient.interceptors.request.use(
   }
 );
 
-//interceptor de respuesta -> se ejecuta antes de que se resuelva la promesa de la solicitud
+//response interceptor -> it executes before the response promise is resolved
 httpClient.interceptors.response.use(
   (response) => response,
   (err) => {
-    //se manejan los errores de respuesta
     const customError = new Error();
     const error = err as AxiosError;
 
-    //*error de respuesta: cuando el cliente recibe una respuesta de error (codigos HTTP 5xx o 4xx)
+    //response error: when the client receives an error response (HTTP 5xx or 4xx status codes)
     if (error.response) {
       const method = error.response.config.method;
 
-      //se asigna un mensaje de error personalizado dependiendo del método de la solicitud
       switch (method) {
         case 'get':
-          customError.message = `No pudimos cargar los datos, inténtelo más tarde`;
+          customError.message = "We couldn't retrieve the data, please try again later";
           break;
         case 'post':
         case 'put':
         case 'patch':
         case 'delete':
-          customError.message = `No pudimos procesar la solicitud, inténtelo más tarde`;
+          customError.message = "We couldn't process your request, please try again later";
           break;
         default:
-          customError.message = `Algo no salió como esperábamos, inténtelo más tarde`;
+          customError.message = 'We encountered an error, please try again later';
       }
     }
 
-    //*error de solicitud: cuando el cliente nunca recibe una respuesta o el request falló
+    //request error: when the client never receives a response, or the request never left
     else if (error.request) {
-      customError.message = `Parece que hay un problema con la conexión, inténtelo más tarde`;
+      customError.message = `It seems that we couldn't establish a connection, please try again later`;
     }
 
-    //*otros errores
+    //other errors
     else {
       console.error(err);
-      customError.message = 'Ocurrió un error inesperado, por favor inténtelo más tarde';
+      customError.message = 'An unexpected error occurred, please try again later';
     }
 
-    //se rechaza la promesa con el mensaje de error personalizado
+    //return the error
     return Promise.reject(customError);
   }
 );
