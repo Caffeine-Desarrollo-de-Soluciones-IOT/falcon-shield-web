fcb69fa1b63460d6581e551887e4927da5ac9e72
GabySol26 <U202113876@upc.edu.pe>
2024-10-26 11:29:21 -0500

    fix: package and main

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,9 @@ import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
-<<<<<<< HEAD
-
-
-import '@/assets/styles.scss'
-import '@/assets/tailwind.css'
-=======
 import '@/assets/styles.scss';
 import '@/assets/tailwind.css';
->>>>>>> 36b8f26f93a34ee829877111f94070909f5d3c3c
+
 
 const app = createApp(App);
 const pinia = createPinia();
