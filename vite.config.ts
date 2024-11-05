8c5a9c69399194944107d6f8cc2aa3476b7acb3e
miikuru002 <38484391+miikuru002@users.noreply.github.com>
2024-11-05 07:27:26 -0500

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js',
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
           dest: 'js'
         }
       ]
