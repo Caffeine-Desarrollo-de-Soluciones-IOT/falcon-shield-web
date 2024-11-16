b884a0ca989bd2505487871cd45167cd293f4a17
miikuru002 <jamutaqortegavelez@gmail.com>
2024-11-16 14:42:29 -0500

    refactor: update language selection in AppTopbar component

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useI18n } from 'vue-i18n';  // Importar useI18n para gestionar el idioma
+import { ref, watchEffect } from 'vue';
 import { AuthService } from '@/service/AuthService';
 import { useLayout } from '@/layout/composables/layout';
 import type { MenuItem } from 'primevue/menuitem';
+import { useI18n } from 'vue-i18n';
 
 const { onMenuToggle, toggleDarkMode, isDarkTheme } = useLayout();
-const { locale } = useI18n();  // Accedemos a la funcionalidad de cambio de idioma
+const { locale } = useI18n();
 
-// Estado para el idioma actual
-const currentLanguage = ref({ name: 'English', code: 'UK' });
-const languages = ref([
-  { name: 'English', code: 'UK' },
-  { name: 'Español', code: 'ES' },
-  { name: 'Français', code: 'FR' }
+const selectedLocale = ref({ name: 'EN', flagCode: 'UK', value: 'en' });
+const locales = ref([
+  { name: 'EN', flagCode: 'UK', value: 'en' },
+  { name: 'ES', flagCode: 'ES', value: 'es' },
+  { name: 'FR', flagCode: 'FR', value: 'fr' },
 ]);
 
 const menu = ref();
@@ -34,17 +33,14 @@ const overlayMenuItems = ref<MenuItem[]>([
   }
 ]);
 
-const changeLanguage = (event: any) => {
-  const language = event.value;  // Obtiene el valor seleccionado del evento
-  currentLanguage.value = language;
-  locale.value = language.code;  // Cambia el idioma en i18n
-};
-
-
-
 function toggleMenu(event: MouseEvent) {
   menu.value.toggle(event);
 }
+
+//i18n change locale
+watchEffect(() => {
+  locale.value = selectedLocale.value.value;
+});
 </script>
 
 <template>
@@ -61,15 +57,15 @@ function toggleMenu(event: MouseEvent) {
 
     <div class="layout-topbar-actions">
       <Button as="router-link" label="Go Premium" icon="pi pi-star"
-              class="p-button-rounded border-0 ml-4 font-light leading-tight custom-button" to="/pricing" />
+        class="p-button-rounded border-0 ml-4 font-light leading-tight custom-button" to="/pricing" />
 
       <!-- Dropdown para seleccionar el idioma -->
-      <Select v-model="currentLanguage" :options="languages" optionLabel="name" checkmark @change="changeLanguage">
+      <Select v-model="selectedLocale" :options="locales" optionLabel="name" checkmark>
         <template #value="slotProps">
           <div v-if="slotProps.value" class="flex items-center">
             <img :alt="slotProps.value.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                 :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
-            <div>{{ slotProps.value.code }}</div>
+              :class="`mr-2 flag flag-${slotProps.value.flagCode.toLowerCase()}`" style="width: 18px" />
+            <div>{{ slotProps.value.name }}</div>
           </div>
           <span v-else>
             {{ slotProps.placeholder }}
@@ -78,8 +74,8 @@ function toggleMenu(event: MouseEvent) {
         <template #option="slotProps">
           <div class="flex items-center">
             <img :alt="slotProps.option.label"
-                 src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                 :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+              src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+              :class="`mr-2 flag flag-${slotProps.option.flagCode.toLowerCase()}`" style="width: 18px" />
             <div>{{ slotProps.option.name }}</div>
           </div>
         </template>
@@ -95,7 +91,7 @@ function toggleMenu(event: MouseEvent) {
       </div>
 
       <button class="layout-topbar-menu-button layout-topbar-action"
-              v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }">
+        v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }">
         <i class="pi pi-ellipsis-v"></i>
       </button>
 
@@ -123,6 +119,7 @@ function toggleMenu(event: MouseEvent) {
   border: none !important;
   transition: all 0.3s ease;
 }
+
 .custom-button:hover {
   background: linear-gradient(45deg, #fbbf24, #f59e0b) !important;
   color: #000000 !important;
