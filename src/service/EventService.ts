40fc559b6707ae48686bd3a5ba73b71fcb366589
Elvi98 <elvia.arteaga98@hotmail.com>
2024-11-17 18:59:31 -0500

    add: dashboard

--- src/service/EventService.ts
+++ src/service/EventService.ts
@@ -5,10 +5,20 @@ import type { IDeviceEvent, IRegisterEventRequestDto } from '@/interfaces/events
 const serviceName = '/events';
 
 export const EventService = {
-  async getEventsByPropertyId(propertyId: number): Promise<IApiResponse<IDeviceEvent[]>> {
-    const response = await httpClient.get<IApiResponse<IDeviceEvent[]>>(`${serviceName}/all/${propertyId}`);
+  async getEvents(userId: number): Promise<IApiResponse<IDeviceEvent[]>> {
+    const response = await httpClient.get<IApiResponse<IDeviceEvent[]>>(
+      `${serviceName}/user/${userId}`
+    );
     return response.data;
   },
+
+  async getEventsByPropertyId(propertyId: number): Promise<IApiResponse<IDeviceEvent[]>> {
+    const response = await httpClient.get<IApiResponse<IDeviceEvent[]>>(
+      `${serviceName}/all/${propertyId}`
+    );
+    return response.data;
+  },
+
   async createEvent(request: IRegisterEventRequestDto): Promise<IApiResponse> {
     const response = await httpClient.post<IApiResponse>(`${serviceName}/create`, request);
     return response.data;
